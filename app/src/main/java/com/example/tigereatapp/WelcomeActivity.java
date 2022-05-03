204a70fd76056fb57a3fea0ff62ab12d8cb69c21
Jimmyluluu <luu21@kimo.com>
2022-05-03 10:28:32 +0800

    fix

--- app/src/main/java/com/example/tigereatapp/WelcomeActivity.java
+++ app/src/main/java/com/example/tigereatapp/WelcomeActivity.java
@@ -2,13 +2,46 @@ package com.example.tigereatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button btnCostomer;
+    Button btnDeliver;
+    Button btnSupplier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        btnCostomer = findViewById(R.id.btn_costomer);
+        btnDeliver = findViewById(R.id.btn_deliver);
+        btnSupplier = findViewById(R.id.btn_supplier);
+
+        btnCostomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, CostomerLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDeliver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+            }
+        });
+
+        btnSupplier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+            }
+        });
     }
 }
