2a051f8b89e6b9f91adc771f52e77744283129d5
fcu-d0843778 <D0843778@o365.fcu.edu.tw>
2022-05-17 03:19:07 +0800

    一點點首頁

---
+++ app/src/main/java/com/example/tigereatapp/HomeActivity.java
@@ -0,0 +1,15 @@
+//首頁
+package com.example.tigereatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
