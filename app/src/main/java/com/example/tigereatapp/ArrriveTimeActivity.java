0eaf215a99464e9515124443e73b08b72bf3649e
Jimmyluluu <luu21@kimo.com>
2022-05-23 00:29:19 +0800

    到達時間

---
+++ app/src/main/java/com/example/tigereatapp/ArrriveTimeActivity.java
@@ -0,0 +1,14 @@
+package com.example.tigereatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArrriveTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arrrive_time);
+    }
+}
