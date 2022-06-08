f6c92f029b9caa87b1f6ae688c95cf2510e6e075
Jimmyluluu <luu1122333@gmail.com>
2022-06-08 23:01:48 +0800

    將跳轉完成

--- app/src/main/java/com/example/tigereatapp/MainActivity.java
+++ app/src/main/java/com/example/tigereatapp/MainActivity.java
@@ -2,26 +2,52 @@ package com.example.tigereatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btnInfo;
-
+    private Button Home;
+    private Button Shop;
+    private Button Search;
+    private Button Set;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_main);
+        Home = findViewById(R.id.Home);
+        Shop = findViewById(R.id.Shopping);
+        Search = findViewById(R.id.Search);
+        Set = findViewById(R.id.Setting);
 
-        btnInfo = findViewById(R.id.btnInfo);
-        btnInfo.setOnClickListener(new View.OnClickListener() {
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        MainActivity.this, ShoppingcartActivity.class);
+                startActivity(intent);
+            }
+        });
+        Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Set.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(
