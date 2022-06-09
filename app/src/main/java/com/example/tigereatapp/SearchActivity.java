ba4fb0948e8bd9bf723bd420971d8e8a923093b2
fcu-d0843778 <D0843778@o365.fcu.edu.tw>
2022-06-10 03:16:12 +0800

    search 裡的 gridView 終於出來了QAQ

--- app/src/main/java/com/example/tigereatapp/SearchActivity.java
+++ app/src/main/java/com/example/tigereatapp/SearchActivity.java
@@ -2,13 +2,21 @@ package com.example.tigereatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -16,12 +24,13 @@ public class SearchActivity extends AppCompatActivity {
     private Button Shop;
     private Button Search;
     private Button Set;
-    private int image[] = {R.drawable.rest1, R.drawable.rest2, R.drawable.rest3, R.drawable.rest4
+
+    private int images[] = {R.drawable.rest1, R.drawable.rest2, R.drawable.rest3, R.drawable.rest4
             , R.drawable.rest5, R.drawable.rest6, R.drawable.rest7
             , R.drawable.rest8, R.drawable.rest9, R.drawable.rest10};
-    private String[] name =  {"茶湯會", "鼎王", "瓦城", "烏龍麵所", "鼎泰豐", "公益麵攤", "肯德基"
+    private String[] names =  {"茶湯會", "鼎王", "瓦城", "烏龍麵所", "鼎泰豐", "公益麵攤", "肯德基"
             , "築間", "壽司郎", "翰林茶坊"};
-    GridView gridView = findViewById(R.id.gv);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,5 +76,11 @@ public class SearchActivity extends AppCompatActivity {
         });
 
 
+        SearchAdapter adapter = new SearchAdapter(SearchActivity.this, names, images);
+        GridView grid = (GridView) findViewById(R.id.grid);
+        grid.setAdapter(adapter);
+
     }
+
+
 }
