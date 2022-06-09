2696766bcb7c8b1c27c0426b6fcc0f313ea8ccc7
Jimmyluluu <luu21@kimo.com>
2022-06-09 21:17:58 +0800

    fix

--- app/src/main/java/com/example/tigereatapp/HomeItem.java
+++ app/src/main/java/com/example/tigereatapp/HomeItem.java
@@ -1,10 +1,9 @@
 //用來get 主要餐廳layout的變數
 package com.example.tigereatapp;
 
-<<<<<<< HEAD
+
 import android.content.Context;
-=======
->>>>>>> ebfd5de03afe4faab2287ba92c11eafe3823764a
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,9 +22,8 @@ public class HomeItem extends BaseAdapter {
     private final int[] resImg;
 
 
-
     //初始化
-    public HomeItem(Context context , String[] resName, String[] resTime, String[] resFee, String[] resScore, int[] resImg){
+    public HomeItem(Context context, String[] resName, String[] resTime, String[] resFee, String[] resScore, int[] resImg) {
         this.context = context;
         this.resName = resName;
         this.resTime = resTime;
@@ -55,44 +53,24 @@ public class HomeItem extends BaseAdapter {
         LayoutInflater layoutInflater =
                 (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         //當ListView被拖拉時會不斷觸發getView，為了避免重複加載必須加上這個判斷
-<<<<<<< HEAD
+
         if (convertView == null) {
             holder = new View(context);
             holder = layoutInflater.inflate(R.layout.home_layout, null);
             TextView reName = (TextView) holder.findViewById(R.id.restName);
             TextView reFee = (TextView) holder.findViewById(R.id.restFee);
             TextView reTime = (TextView) holder.findViewById(R.id.restTime);
-            TextView reScore = (TextView) holder.findViewById(R.id.restScore);
+            TextView reScore = (TextView) holder.findViewById(R.id.reatScore);
             ImageView reImg = (ImageView) holder.findViewById(R.id.restImg);
             reName.setText(resName[position]);
             reFee.setText(resFee[position]);
             reTime.setText(resTime[position]);
             reScore.setText(resScore[position]);
             reImg.setImageResource(resImg[position]);
-=======
-        if (view == null) {
-            holder = new ViewHolder();
-            view = inflater.inflate(R.layout.activity_main, null);
-            holder.restName = (TextView) view.findViewById(R.id.restName);
-            holder.restFee = (TextView) view.findViewById(R.id.restFee);
-            holder.restTime = (TextView) view.findViewById(R.id.restTime);
-            holder.restScore = (TextView) view.findViewById(R.id.reatScore);
-            holder.restImg = (ImageView) view.findViewById(R.id.restImg);
-            view.setTag(holder);
-        } else {
-            holder = (ViewHolder) view.getTag();
-        }
-        //1=都市 2=政府所在地
-        //不同類型用不同Style的表現方式
-        /*if (ElementsData[i][0].equals("1")){
-            holder.Local.setText("★");
-            holder.Name.setText(ElementsData[i][1]);
-            holder.rlBorder.setBackgroundColor(Color.parseColor("#FFDBC9"));
->>>>>>> ebfd5de03afe4faab2287ba92c11eafe3823764a
-
         } else {
             holder = (View) convertView;
         }
         return holder;
+
     }
 }
