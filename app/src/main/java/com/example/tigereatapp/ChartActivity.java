e92cfbd3a29004c6939238551193fa878d4833f7
fcu-d0843778 <D0843778@o365.fcu.edu.tw>
2022-06-09 22:58:59 +0800

    chart OKKK

--- app/src/main/java/com/example/tigereatapp/ChartActivity.java
+++ app/src/main/java/com/example/tigereatapp/ChartActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ChartActivity extends AppCompatActivity {
+
+    public static final String SHOP_NO = "shop_no";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +25,27 @@ public class ChartActivity extends AppCompatActivity {
 
         Intent intent = this.getIntent();
         chartList.add(new Chart(R.drawable.rest1, "茶湯會", "5.0"));
-        chartList.add(new Chart(R.drawable.rest2, "鼎王", "4.8"));
-        chartList.add(new Chart(R.drawable.rest3, "瓦城", "4.9"));
+        chartList.add(new Chart(R.drawable.rest2, "鼎王", "4.9"));
+        chartList.add(new Chart(R.drawable.rest3, "瓦城", "4.8"));
         chartList.add(new Chart(R.drawable.rest4, "烏龍麵所", "4.5"));
-        chartList.add(new Chart(R.drawable.rest5, "鼎泰豐", "4.7"));
+        chartList.add(new Chart(R.drawable.rest5, "鼎泰豐", "4.0"));
 
 
-        ChartArrayAdapter adapter = new ChartArrayAdapter(this, R.layout.chart_layout, chartList);
+        ChartArrayAdapter adapter = new ChartArrayAdapter(this,
+                R.layout.chart_layout, chartList);
         listView.setAdapter(adapter);
+
+        //偵測被按到餐廳
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(ChartActivity.this, MealActivity.class);
+                intent.putExtra(SHOP_NO, position);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
