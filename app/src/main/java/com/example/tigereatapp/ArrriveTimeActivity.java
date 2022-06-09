25325bbf0dd7282135332935e0754d86ff0537a0
Jimmyluluu <luu21@kimo.com>
2022-06-09 23:28:45 +0800

    fix payment

--- app/src/main/java/com/example/tigereatapp/ArrriveTimeActivity.java
+++ app/src/main/java/com/example/tigereatapp/ArrriveTimeActivity.java
@@ -2,13 +2,20 @@ package com.example.tigereatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class ArrriveTimeActivity extends AppCompatActivity {
 
+    private Button Home;
+    private Button Shop;
+    private Button Search;
+    private Button Set;
 
     TextView showTime;
     Calendar mCal = Calendar.getInstance();
@@ -18,7 +25,11 @@ public class ArrriveTimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arrrive_time);
         showTime = findViewById(R.id.showTime);
-
+        Home = findViewById(R.id.Home);
+        Shop = findViewById(R.id.Shopping);
+        Search = findViewById(R.id.Search);
+        Set = findViewById(R.id.Setting);
+        Home = findViewById(R.id.Home);
 
         int nowHour = mCal.get(Calendar.HOUR);
         int nowMin = mCal.get(Calendar.MINUTE);
@@ -32,5 +43,39 @@ public class ArrriveTimeActivity extends AppCompatActivity {
         }
 
         showTime.setText(String.valueOf(nowHour) + "點" + String.valueOf(nowMin) + "分" );
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        ArrriveTimeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        ArrriveTimeActivity.this, ShoppingcartActivity.class);
+                startActivity(intent);
+            }
+        });
+        Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        ArrriveTimeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        ArrriveTimeActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tigereatapp/PaymentActivity.java
+++ app/src/main/java/com/example/tigereatapp/PaymentActivity.java
@@ -15,7 +15,9 @@ public class PaymentActivity extends AppCompatActivity{
     private Button Shop;
     private Button Search;
     private Button Set;
-    Button bt1;
+    private Button mobilePayment;
+    private Button cashPayment;
+    private Button cencel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,106 +26,87 @@ public class PaymentActivity extends AppCompatActivity{
         Shop = findViewById(R.id.Shopping);
         Search = findViewById(R.id.Search);
         Set = findViewById(R.id.Setting);
-        bt1 = (Button) findViewById(R.id.cashPayment);
+        cashPayment = (Button) findViewById(R.id.cashPayment);
+        mobilePayment = (Button) findViewById(R.id.mobilePayment);
+        cencel = (Button) findViewById(R.id.button3);
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        PaymentActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        PaymentActivity.this, ShoppingcartActivity.class);
+                startActivity(intent);
+            }
+        });
+        Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        PaymentActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        PaymentActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
+        cashPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        PaymentActivity.this, ShoppingcartActivity.class);
+                startActivity(intent);
+            }
+        });
+        cashPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        PaymentActivity.this, ShoppingcartActivity.class);
+                startActivity(intent);
+            }
+        });
+        cashPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "已選擇現金交易", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(
+                        PaymentActivity.this, ShoppingcartActivity.class);
+                startActivity(intent);
+            }
+        });
+        mobilePayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        PaymentActivity.this, MobilePayActivity.class);
+                startActivity(intent);
+            }
+        });
+        cencel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        PaymentActivity.this, ShoppingcartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
-
-    protected void cashPay(Bundle savedInstanceState) {
-        //創建監聽器 綁定Button資源
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment);
-
-
-        bt1 = (Button) findViewById(R.id.cashPayment);
-        //設置Button監聽
-        bt1.setOnClickListener(new MyButtonListener());
-    }
-
-    //實現監聽器(需要導入android.view.View和android.content.Intent)
-    // 實現OnClickListener接口
-    private class MyButtonListener implements View.OnClickListener {
-        @Override
-
-        public void onClick(View view) {
-            Toast.makeText(getApplicationContext(), "已選擇現金交易", Toast.LENGTH_LONG).show();
-
-            Intent intent = new Intent();
-            intent.setClass(PaymentActivity.this, ShoppingcartActivity.class);//從MainActivity頁面跳轉至LoginActivity頁面
-            startActivity(intent);
-            PaymentActivity.this.finish();
-            Home.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(
-                            PaymentActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-            Shop.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(
-                            PaymentActivity.this, ShoppingcartActivity.class);
-                    startActivity(intent);
-                }
-            });
-            Search.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(
-                            PaymentActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-            Set.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(
-                            PaymentActivity.this, InfoActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-            Home.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(
-                            PaymentActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-            Shop.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(
-                            PaymentActivity.this, ShoppingcartActivity.class);
-                    startActivity(intent);
-                }
-            });
-            Search.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(
-                            PaymentActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-            Set.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(
-                            PaymentActivity.this, InfoActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
-
-    }
 
     protected void backToCart(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
