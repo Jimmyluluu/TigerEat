72dfd8697a2a0e2c2fb74b709d9e66db48f0e8bc
Jimmyluluu <luu21@kimo.com>
2022-06-09 18:09:59 +0800

    home

--- app/src/main/java/com/example/tigereatapp/HomeItem.java
+++ app/src/main/java/com/example/tigereatapp/HomeItem.java
@@ -1,8 +1,18 @@
 //用來get 主要餐廳layout的變數
 package com.example.tigereatapp;
 
-public class HomeItem {
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+public class HomeItem extends BaseAdapter {
+
+    /*
     private int restImg;
     private String restName;
     private String restTime;
@@ -16,25 +26,72 @@ public class HomeItem {
         this.restTime = restTime;
         this.restFee = restFee;
         this.restScore = restScore;
+    }*/
+    private String[][] ElementsData ;   //資料
+    private LayoutInflater inflater;    //加載layout
+    private int indentionBase;          //item缩排
+    //優化Listview 避免重新加載
+    //這邊宣告你會動到的Item元件
+    static class ViewHolder{
+        LinearLayout rlBorder;
+        TextView restName;
+        TextView restTime;
+        TextView restFee;
+        TextView restScore;
+        ImageView restImg;
     }
 
-    public int getRestImg() {
-        return restImg;
+    //初始化
+    public void ViewAdapter(String[][] data, LayoutInflater inflater){
+        this.ElementsData = data;
+        this.inflater = inflater;
+        indentionBase = 100;
     }
 
-    public String getRestName() {
-        return restName;
+    @Override
+    public int getCount() {
+        return ElementsData.length;
     }
 
-    public String getRestTime() {
-        return restTime;
+    @Override
+    public Object getItem(int i) {
+        return ElementsData[i];
     }
 
-    public String getRestFee() {
-        return restFee;
+    @Override
+    public long getItemId(int i) {
+        return i;
     }
 
-    public String getRestScore() {
-        return restScore;
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+        //當ListView被拖拉時會不斷觸發getView，為了避免重複加載必須加上這個判斷
+        if (view == null) {
+            holder = new ViewHolder();
+            view = inflater.inflate(R.layout.activity_main, null);
+            holder.restName = (TextView) view.findViewById(R.id.restName);
+            holder.restFee = (TextView) view.findViewById(R.id.restFee);
+            holder.restTime = (TextView) view.findViewById(R.id.restTime);
+            holder.restScore = (TextView) view.findViewById(R.id.restScore);
+            holder.restImg = (ImageView) view.findViewById(R.id.restImg);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        //1=都市 2=政府所在地
+        //不同類型用不同Style的表現方式
+        /*if (ElementsData[i][0].equals("1")){
+            holder.Local.setText("★");
+            holder.Name.setText(ElementsData[i][1]);
+            holder.rlBorder.setBackgroundColor(Color.parseColor("#FFDBC9"));
+
+        }else{
+            holder.Local.setText("");
+            holder.Name.setText(ElementsData[i][1]);
+            LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) holder.rlBorder.getLayoutParams();
+            lp.setMargins(indentionBase,0, 0,0);//縮牌
+        }*/
+        return view;
     }
 }
--- app/src/main/java/com/example/tigereatapp/MainActivity.java
+++ app/src/main/java/com/example/tigereatapp/MainActivity.java
@@ -2,16 +2,32 @@ package com.example.tigereatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.bumptech.glide.request.transition.Transition;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button Home;
     private Button Shop;
     private Button Search;
     private Button Set;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
         Shop = findViewById(R.id.Shopping);
         Search = findViewById(R.id.Search);
         Set = findViewById(R.id.Setting);
+
+        String[] str = {"茶湯會","台北市","台中市","台南市","高雄市"};
+        ListView listview = (ListView) findViewById(R.id.listview);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                str);
+        listview.setAdapter(adapter);
+
+
 
         Home.setOnClickListener(new View.OnClickListener() {
             @Override
